9c243ce2ec84a370a0304ce58b9bba943b2b0506
Aditya Choudhari <adityachoudhari@Adityas-MacBook-Pro.local>
2021-09-20 15:11:43 -0400

    setting up socket connctions on backend

--- index.js
+++ index.js
@@ -1,22 +1,44 @@
-import app from "express";
-import { createServer } from "http";
+/*
+* This is the backend portion of the project that deals with server connections for the chat app
+*/
+
+const app = require("express")();
+const server = require("http").createServer(app);
 import cors from "cors";
-import io from "socket.io";
 
-const App = app();
-const Server = createServer(App);
+const io = require("socket.io")(server, {
+	cors: {
+		origin: "*",
+		methods: [ "GET", "POST" ]
+	}
+});
 
-const IO = io(Server, { cors: {
-  origin: "*",
-  methods: ["GET", "POST"]
-}})
-
-App.use(cors());
+app.use(cors());
 
 const PORT = process.env.PORT || 5000;
 
-App.get("/", (req, res) => {
-  res.send(`Server is running`);
+app.get('/', (req, res) => {
+	res.send('Running');
 });
 
-Server.listen(PORT, () => console.log(`Server is listening on port: ${PORT}`));
+io.on("connection", (socket) => {
+  // emit socket id upon connection
+	socket.emit("me", socket.id);
+
+  // broadcast a call ended message upon disconnection
+	socket.on("disconnect", () => {
+		socket.broadcast.emit("callEnded")
+	});
+
+  // emit necessary data upon call user event
+	socket.on("callUser", ({ userToCall, signalData, from, name }) => {
+		io.to(userToCall).emit("callUser", { signal: signalData, from, name });
+	});
+
+  // emit data signal upon answering call
+	socket.on("answerCall", (data) => {
+		io.to(data.to).emit("callAccepted", data.signal)
+	});
+});
+
+server.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
